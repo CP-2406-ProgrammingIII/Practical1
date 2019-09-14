930ccdb70b27b5717ac7ac3a9e29835dc4d09ccc
JosephQQQ <elmoukhtar.mokhtari@my.jcu.edu.au>
2019-09-14 19:06:41 +0800

    Trying this out

---
+++ src/First.java
@@ -0,0 +1,7 @@
+/** This is the beginning introduction*/
+
+public class First {
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/FirstDialog.java
@@ -0,0 +1,9 @@
+/** this is a window popup */
+
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+    public static void main (String[] args) {
+        JOptionPane.showMessageDialog(null, "Issa popup popping");
+    }
+}
